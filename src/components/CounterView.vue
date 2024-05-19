62a89e6207db3dabe578eb748f56d6cef2e7b16b
Abdullah Elamir <abdullah.el_ameer@yahoo.com>
2024-05-19 23:53:54 +0200

    Fix some code

--- src/components/CounterView.vue
+++ src/components/CounterView.vue
@@ -19,7 +19,7 @@ export default {
     <span type="button" class="btn btn-outline-info coun" >
     {{ Counter }}
     </span>
-    <button type="button" class="btn btn-outline-info coun" @click="Counter=0">
+    <button type="button" class="btn btn-outline-info coun" @click="Counter = Counter - 1">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"/>
 </svg>
